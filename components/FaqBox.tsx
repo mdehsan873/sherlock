4bb8ba8f2bbfc5534d15ed377e48b86daba58d5c
Mohammad Asad <smasad@amazon.com>
2025-05-25 20:56:25 +0530

    Fixed accordion

--- components/FaqBox.tsx
+++ components/FaqBox.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Pressable, Text, View} from 'react-native';
+import {icons} from "@/constants/icons";
 
 interface FaqBoxProps {
     question: string;
     answer: string;
 }
 
-// TODO Make this Accordian
+// TODO Add animation
 const FaqBox = ({ question, answer }: FaqBoxProps) => {
+    const [isOpen, setIsOpen] = React.useState(false);
     return (
-        <View className="p-4 border border-solid border-light rounded-lg h-full w-full">
-            <Text className="text-base font-semibold text-secondary mb-3">{question}</Text>
-            <Text className="text-sm text-accent leading-normal">{answer}</Text>
-        </View>
+        <Pressable
+        onPress={() => setIsOpen(!isOpen) }
+        >
+            <View className="p-4 border border-solid border-light rounded-lg h-full w-full">
+                <View className="flex-row justify-between items-center px-2">
+                    <Text className="text-base font-semibold text-secondary mb-2">{question}</Text>
+                    <Image
+                        source={icons.accordion}
+                        style={{
+                            maxWidth: 20,
+                            maxHeight: 20,
+                            transform: [{rotate : isOpen ? '180deg' : '0deg' }]
+                        }}
+                    />
+                </View>
+
+                { isOpen &&
+                    <View>
+                        <Text className="text-sm text-accent leading-normal px-2">{answer}</Text>
+                    </View>
+                }
+            </View>
+        </Pressable>
     );
 };
 
