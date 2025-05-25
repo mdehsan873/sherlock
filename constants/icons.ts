4c9bd8ef4b46c77bcd5a7b1e08fabdc32c41484d
Mohammad Asad <smasad@amazon.com>
2025-05-25 19:28:43 +0530

    Added new icons

--- constants/icons.ts
+++ constants/icons.ts
@@ -4,4 +4,7 @@ export const icons = {
     message: require('../assets/icons/message.png'),
     time: require('../assets/icons/time.png'),
     verified: require('../assets/icons/verified.png'),
+    whatsapp: require('../assets/icons/whatsapp.png'),
+    tick: require('../assets/icons/tick.png'),
+    accordion: require('../assets/icons/accordion.png'),
 };
